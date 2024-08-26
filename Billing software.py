21d2b6c3cae0e3eb19e605327e4ab6335290f2d8
ItzAiluropoda <aahilmohammed59@gmail.com>
2024-08-26 19:41:19 +0530

    error handling

--- Billing software.py
+++ Billing software.py
@@ -20,40 +20,44 @@ def NEWBILL():
             print("Dish not Found")
     d={"Name":N,"Phone Number":P,"Payment Method":m,"Total":t}
 d={}
-s={"cake":70, "Juice":30, "Milkshake":60, "Rice":80, "Dal":110, "Snack":20}
+s={"cake":70, "juice":30, "milkshake":60, "rice":80, "dal":110, "snack":20}
 o={}
 def MENU():
     global s
     while True:
-        print('''To add new item press 1
-        To remove Item press 2
-        To stop press 3 ''')
-        l=int(input("Enter your selection:"))
-        if l==1:
-            n=str(input("Enter Dish Name:"))
-            p=int(input("Enter Dish Price:"))
-            s.update({n.lower():p})
-            print("Dish added")
-        elif l==2:
-            j=str(input("Enter name of Dish you wish to remove:"))
-            if j.lower() in s:
-                s.pop(j.lower())
-                print("Dish removed")
-            else:
-                print("Dish not found")
-        elif l==3:
-            print(s)
-            break
+        try:
+            print("To add new item press 1 \nTo remove Item press 2 \nTo stop press 3 ")
+            l=int(input("Enter your selection:"))
+            if l==1:
+                n=str(input("Enter Dish Name:"))
+                p=int(input("Enter Dish Price:"))
+                s.update({n.lower():p})
+                print("Dish added")
+            elif l==2:
+                j=str(input("Enter name of Dish you wish to remove:"))
+                if j.lower() in s:
+                    s.pop(j.lower())
+                    print("Dish removed")
+                else:
+                    print("Dish not found")
+            elif l==3:
+                print(s)
+                break
+        except:
+            print("Wrong input try again")
 while True:
-    print("Enter 1 to order \n Enter 2 change menu \n Enter 3 to print Bill \nEnter 4 to Quit")
-    j=int(input("Enter:"))
-    if j==1:
-        NEWBILL()
-    elif j==2:
-        MENU()
-    elif j==3:
-        print("Print system not setup")
-    elif j==4:
-        break
-    else:
+    try:
+        print("Enter 1 to order \nEnter 2 change menu \nEnter 3 to print Bill \nEnter 4 to Quit")
+        j=int(input("Enter:"))
+        if j==1:
+            NEWBILL()
+        elif j==2:
+            MENU()
+        elif j==3:
+            print("Print system not setup")
+        elif j==4:
+            break
+        else:
+            print("Wrong input try again")
+    except:
         print("Wrong input try again")
